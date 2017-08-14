22ad3f8be8ad19615e9f529e22f39696569747a0
Juan Hernandez <jiniguez@foundationmedicine.com>
2017-08-14 10:49:38 -0500

    adding a static sql query

--- jdbc/ch_01/src/com/lynda/training/Main.java
+++ jdbc/ch_01/src/com/lynda/training/Main.java
@@ -1,5 +1,6 @@
 package com.lynda.training;
 
+import com.sun.org.apache.regexp.internal.RE;
 import com.sun.tools.internal.ws.wsdl.document.soap.SOAPUse;
 
 import java.sql.*;
@@ -10,8 +11,9 @@ public class Main {
     public static final String CONN_STRING = "jdbc:hsqldb:data/explorecalifornia";
     public static final String USER = "dbuser";
     public static final String PASSWORD = "dbpassword";
+    public static final String SQL = "select * from STATES";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 	// write your code here
         Connection conn = null;
         Statement stmt = null;
@@ -20,9 +22,21 @@ public class Main {
         try {
             conn = DriverManager.getConnection(CONN_STRING, USER, PASSWORD);
             System.out.println("successfully connected");
+
+            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            rs = stmt.executeQuery(SQL);
+
+            rs.last();
+            System.out.println("number of rows: " + rs.getRow());
+
+
         } catch (SQLException e) {
             System.err.println(e.getMessage());
             System.err.println(e.getErrorCode());
+        } finally {
+            if(conn != null){
+                conn.close();
+            }
         }
     }
 }
