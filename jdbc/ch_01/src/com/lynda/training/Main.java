5c26ba30f9c7406227087091f26cb8da8803957d
Juan Hernandez <jiniguez@foundationmedicine.com>
2017-08-14 09:53:40 -0500

    initial  connection

---
+++ jdbc/ch_01/src/com/lynda/training/Main.java
@@ -0,0 +1,26 @@
+package com.lynda.training;
+
+import com.sun.tools.internal.ws.wsdl.document.soap.SOAPUse;
+
+import java.sql.*;
+
+public class Main {
+
+    public static final String CONN_STRING = "jdbc:mysql://localhost/explorecalifornia";
+    public static final String USER = "dbuser";
+    public static final String PASSWORD = "dbpassword";
+
+    public static void main(String[] args) {
+	// write your code here
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DriverManager.getConnection(CONN_STRING, USER, PASSWORD);
+            System.out.println("successfully connected");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
